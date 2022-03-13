0ec787043292a38ae50fcd88ea885e09aedfa641
LinhChi <linhchitwt@gmail.com>
2022-03-13 14:24:50 +0700

    Update Display2.cs

--- ManagementSoftware/Display2.cs
+++ ManagementSoftware/Display2.cs
@@ -22,6 +22,8 @@ namespace ManagementSoftware
             Display1 display1 = new Display1();
             display1.Show();
             this.Hide();
+            ///////////
+            ///sahk
         }
 
         private void X_dis2_Click(object sender, EventArgs e)
