7f55d9afc9cdaa70d20392632ddbc9c34af157c4
LinhChi <linhchitwt@gmail.com>
2022-03-14 08:42:50 +0700

    new 14/3

--- ManagementSoftware/Admin_ST.Designer.cs
+++ ManagementSoftware/Admin_ST.Designer.cs
@@ -29,10 +29,22 @@ namespace ManagementSoftware
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin_ST));
+            this.SuspendLayout();
+            // 
+            // Admin_ST
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1000, 640);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Admin_ST";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin_ST";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
