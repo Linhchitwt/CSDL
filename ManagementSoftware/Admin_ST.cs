024f911e8adcde4eee472f560cae5c63419795b7
LinhChi <linhchitwt@gmail.com>
2022-03-15 21:14:30 +0700

    15/3
    full admin

--- ManagementSoftware/Admin_ST.cs
+++ ManagementSoftware/Admin_ST.cs
@@ -16,5 +16,40 @@ namespace ManagementSoftware
         {
             InitializeComponent();
         }
+
+        private void Home_Click(object sender, EventArgs e)
+        {
+            Admin ad = new Admin();
+            ad.Show();
+            this.Hide();
+        }
+
+        private void Manage_Click(object sender, EventArgs e)
+        {
+            manage mng = new manage();
+            mng.Show();
+            this.Hide();
+        }
+
+        private void AD_teach_Click(object sender, EventArgs e)
+        {
+            Admin_TC tc = new Admin_TC();
+            tc.Show();
+            this.Hide();
+        }
+
+        private void AD_emp_Click(object sender, EventArgs e)
+        {
+            Admin_Employee ep = new Admin_Employee();
+            ep.Show();
+            this.Hide();
+        }
+
+        private void guna2Button5_Click(object sender, EventArgs e)
+        {
+            Display3 display3 = new Display3();
+            display3.Show();
+            this.Hide();
+        }
     }
 }
--- ManagementSoftware/Admin_TC.Designer.cs
+++ ManagementSoftware/Admin_TC.Designer.cs
@@ -29,12 +29,150 @@ namespace ManagementSoftware
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin_TC));
+            this.Manage = new Guna.UI2.WinForms.Guna2Button();
+            this.Home = new Guna.UI2.WinForms.Guna2Button();
+            this.AD_emp = new Guna.UI2.WinForms.Guna2Button();
+            this.AD_ST = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button5 = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // Manage
+            // 
+            this.Manage.AccessibleName = "Manage";
+            this.Manage.BackColor = System.Drawing.Color.Transparent;
+            this.Manage.BorderColor = System.Drawing.Color.Transparent;
+            this.Manage.BorderStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
+            this.Manage.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Manage.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Manage.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Manage.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Manage.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Manage.FillColor = System.Drawing.Color.Transparent;
+            this.Manage.FocusedColor = System.Drawing.Color.White;
+            this.Manage.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Manage.ForeColor = System.Drawing.Color.Transparent;
+            this.Manage.Location = new System.Drawing.Point(221, 584);
+            this.Manage.Name = "Manage";
+            this.Manage.Size = new System.Drawing.Size(120, 50);
+            this.Manage.TabIndex = 30;
+            this.Manage.UseTransparentBackground = true;
+            this.Manage.Click += new System.EventHandler(this.Manage_Click);
+            // 
+            // Home
+            // 
+            this.Home.AccessibleName = "Home";
+            this.Home.BackColor = System.Drawing.Color.Transparent;
+            this.Home.BorderColor = System.Drawing.Color.Transparent;
+            this.Home.BorderStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
+            this.Home.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Home.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Home.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Home.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Home.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Home.FillColor = System.Drawing.Color.Transparent;
+            this.Home.FocusedColor = System.Drawing.Color.White;
+            this.Home.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Home.ForeColor = System.Drawing.Color.Transparent;
+            this.Home.Location = new System.Drawing.Point(118, 584);
+            this.Home.Name = "Home";
+            this.Home.Size = new System.Drawing.Size(48, 50);
+            this.Home.TabIndex = 33;
+            this.Home.UseTransparentBackground = true;
+            this.Home.Click += new System.EventHandler(this.Home_Click);
+            // 
+            // AD_emp
+            // 
+            this.AD_emp.AccessibleName = "AD_emp";
+            this.AD_emp.BackColor = System.Drawing.Color.Transparent;
+            this.AD_emp.BorderColor = System.Drawing.Color.Transparent;
+            this.AD_emp.BorderStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
+            this.AD_emp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AD_emp.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.AD_emp.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.AD_emp.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.AD_emp.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.AD_emp.FillColor = System.Drawing.Color.Transparent;
+            this.AD_emp.FocusedColor = System.Drawing.Color.White;
+            this.AD_emp.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.AD_emp.ForeColor = System.Drawing.Color.Transparent;
+            this.AD_emp.Location = new System.Drawing.Point(512, 584);
+            this.AD_emp.Name = "AD_emp";
+            this.AD_emp.Size = new System.Drawing.Size(120, 50);
+            this.AD_emp.TabIndex = 34;
+            this.AD_emp.UseTransparentBackground = true;
+            this.AD_emp.Click += new System.EventHandler(this.AD_emp_Click);
+            // 
+            // AD_ST
+            // 
+            this.AD_ST.AccessibleName = "Admin_ST";
+            this.AD_ST.BackColor = System.Drawing.Color.Transparent;
+            this.AD_ST.BorderColor = System.Drawing.Color.Transparent;
+            this.AD_ST.BorderStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
+            this.AD_ST.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AD_ST.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.AD_ST.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.AD_ST.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.AD_ST.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.AD_ST.FillColor = System.Drawing.Color.Transparent;
+            this.AD_ST.FocusedColor = System.Drawing.Color.White;
+            this.AD_ST.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.AD_ST.ForeColor = System.Drawing.Color.Transparent;
+            this.AD_ST.Location = new System.Drawing.Point(653, 584);
+            this.AD_ST.Name = "AD_ST";
+            this.AD_ST.Size = new System.Drawing.Size(120, 50);
+            this.AD_ST.TabIndex = 35;
+            this.AD_ST.UseTransparentBackground = true;
+            this.AD_ST.Click += new System.EventHandler(this.AD_ST_Click);
+            // 
+            // guna2Button5
+            // 
+            this.guna2Button5.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button5.BorderColor = System.Drawing.Color.Transparent;
+            this.guna2Button5.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.guna2Button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2Button5.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button5.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button5.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button5.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button5.FillColor = System.Drawing.Color.Transparent;
+            this.guna2Button5.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button5.ForeColor = System.Drawing.Color.Transparent;
+            this.guna2Button5.Location = new System.Drawing.Point(832, 583);
+            this.guna2Button5.Name = "guna2Button5";
+            this.guna2Button5.Size = new System.Drawing.Size(44, 45);
+            this.guna2Button5.TabIndex = 36;
+            this.guna2Button5.UseTransparentBackground = true;
+            this.guna2Button5.Click += new System.EventHandler(this.guna2Button5_Click);
+            // 
+            // Admin_TC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1000, 640);
+            this.Controls.Add(this.guna2Button5);
+            this.Controls.Add(this.AD_ST);
+            this.Controls.Add(this.AD_emp);
+            this.Controls.Add(this.Home);
+            this.Controls.Add(this.Manage);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Admin_TC";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin_TC";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2Button Manage;
+        private Guna.UI2.WinForms.Guna2Button Home;
+        private Guna.UI2.WinForms.Guna2Button AD_emp;
+        private Guna.UI2.WinForms.Guna2Button AD_ST;
+        private Guna.UI2.WinForms.Guna2Button guna2Button5;
     }
 }
